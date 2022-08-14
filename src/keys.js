ef672ec10d52132b2c2a908cadc6edc3a85c6365
Ruben <rhernandezce@unprg.edu.pe>
2022-08-13 23:03:17 -0500

    key

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports ={
     database:{
-        host: 'localhost',
-        user: 'root',
-        password: '',
+        host: 'db-hermes.ckdywq4glsir.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'ynhZ4A0Zc3AkaBSqp6Ua',
         database: 'hermesdb'
 
     }
